2282f5e1172b8343fda2612fe2531d6a3a4f632a
LamaTopaz <tam.yong.jun@gmail.com>
2025-08-22 17:43:03 +0800

    LIVE CAM SUPPORT (HAVE TO CHANGE IP MANUALLY)

--- PROJECT 2025/app/src/main/java/com/example/project2025/ui/dashboard/DashboardUserFragment.java
+++ PROJECT 2025/app/src/main/java/com/example/project2025/ui/dashboard/DashboardUserFragment.java
@@ -1,10 +1,15 @@
 package com.example.project2025.ui.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,9 +27,12 @@ import java.net.Socket;
 public class DashboardUserFragment extends Fragment {
 
     private DashboardFragmentUserBinding binding;
-    private Button feedButton;
-    private static final String PI_IP = "192.168.135.157";
-    private static final int PORT = 12345;
+    private LinearLayout feedButton;
+    private WebView liveCam;
+    private static final String PI_IP = "172.20.10.2";
+    private static final int FEED_PORT = 12345;
+    private static final int HTTP_PORT = 8889;
+    private static final String LIVE_FOLDER = "/cam1";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,8 +41,21 @@ public class DashboardUserFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.dashboard_fragment_user, container, false);
 
+        liveCam = root.findViewById(R.id.ipCamera);
         feedButton = root.findViewById(R.id.feedButton);
 
+        return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        WebSettings webSettings = liveCam.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        liveCam.setWebViewClient(new WebViewClient());
+
+        liveCam.loadUrl("http://" + PI_IP + ":" + HTTP_PORT + LIVE_FOLDER);
         feedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,7 +63,7 @@ public class DashboardUserFragment extends Fragment {
             }
         });
 
-        return root;
+
     }
 
     private void sendFeedCommand() {
@@ -50,7 +71,7 @@ public class DashboardUserFragment extends Fragment {
             @Override
             public void run() {
                 try {
-                    Socket socket = new Socket(PI_IP, PORT);
+                    Socket socket = new Socket(PI_IP, FEED_PORT);
                     OutputStream output = socket.getOutputStream();
                     output.write("Feed".getBytes());
                     output.flush();
@@ -70,6 +91,7 @@ public class DashboardUserFragment extends Fragment {
                         @Override
                         public void run() {
                             Toast.makeText(requireContext(), "Failed becauuse of : " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            Log.d("Manual Feed ERROR:", "Failed becauuse of : " + e.getMessage());
                         }
                     });
                 }
