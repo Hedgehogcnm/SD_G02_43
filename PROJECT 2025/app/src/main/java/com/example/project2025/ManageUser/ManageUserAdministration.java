4602eb84dd4f7085e1cbdbb5fedbbde380e5a5ea
LamaTopaz <tam.yong.jun@gmail.com>
2025-09-19 04:27:30 +0800

    NAVIGATION: LIVE CAM AND FEEDER FRAGMENT FOR ADMINISTRATION

--- PROJECT 2025/app/src/main/java/com/example/project2025/ManageUser/ManageUserAdministration.java
+++ PROJECT 2025/app/src/main/java/com/example/project2025/ManageUser/ManageUserAdministration.java
@@ -11,24 +11,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.project2025.R;
+import com.example.project2025.databinding.ActivityMainBinding;
+import com.example.project2025.databinding.ManageUserAdministrationBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ManageUserAdministration extends AppCompatActivity {
 
     private TextView usernameTextView;
     private Button editUserButton;
     private SharedPreferences sharedPreferences;
+    private ManageUserAdministrationBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.manage_user_administration);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        binding = ManageUserAdministrationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.manage_user_admin_dashboard_navigation, R.id.manage_user_admin_feeder_navigation
+        ).build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(binding.navView, navController);
 
         usernameTextView = findViewById(R.id.username);
         editUserButton = findViewById(R.id.edit_button);
